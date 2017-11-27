9a00bb5d38a70eb04fdc11d1c6cea9b3e63e352e
Heba Elayoty (InConsulting Inc.) <v-heelay@microsoft.com>
2017-11-27 13:26:13 -0800

    implement identificationProfileCreateEnrollment API

--- types/speakerRecognition.d.ts
+++ types/speakerRecognition.d.ts
@@ -1,6 +1,13 @@
 export class speakerRecognition {
-	constructor(_ref: any);
-	identificationProfileCreateEnrollment(_ref2: any): any;
+	constructor(options: speakerRecognitionOptions);
+
+	/**
+	 * 
+	 * Enrollment for speaker identification is text-independent, which means that there are no restrictions on what the speaker says in the audio. 
+	 */
+	identificationProfileCreateEnrollment(options: identificationProfileCreateEnrollmentOptions): Promise<identificationProfileCreateEnrollmentReturnValue>;
+
+
 	identificationProfileCreateProfile(_ref3: any): any;
 	identificationProfileDeleteProfile(_ref4: any): any;
 	identificationProfileGet(_ref5: any): any;
@@ -16,4 +23,51 @@ export class speakerRecognition {
 	verificationProfileGetAllProfiles(): any;
 	verificationProfileGetProfile(_ref14: any): any;
 	verificationProfileResetEnrollments(_ref15: any): any;
+
+	
+}
+
+export interface speakerRecognitionOptions {
+	apiKey: string,
+	endpoint: string
+}
+
+//Identification Profile Create Enrollment Options
+export interface identificationProfileCreateEnrollmentOptions {
+	parameters?: identificationProfileCreateEnrollmentParameters,
+	headers?: identificationProfileCreateEnrollmentHeaders
+    body?: string
+}
+
+export interface identificationProfileCreateEnrollmentParameters {
+
+	/**
+	 * ID of speaker identification profile
+	 */
+	identificationProfileId: string,
+
+	/**
+	 * Instruct the service to waive the recommended minimum audio limit needed for enrollment. 
+	 * Set value to “true” to force enrollment using any audio length (min. 1 second).
+	 */
+
+	shortAudio?: boolean
+}
+
+export interface identificationProfileCreateEnrollmentHeaders {
+
+	/**
+	 * Media type of the body sent to the API.
+	 */
+	"Content-Type"?: string,
+
+	/**
+	 * Subscription key which provides access to this API.
+	 */
+	"Ocp-Apim-Subscription-Key"?: string
+}
+
+export interface identificationProfileCreateEnrollmentReturnValue {
+	error?: [{ code : number, message : string }]
+	 
 }
