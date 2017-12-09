8ad1508dead1df449db1bbdf1808ecb3b2d94b63
Jeremy Foster <jeremy.foster@live.com>
2017-12-08 18:55:53 -0800

    remove method doc comments

--- types/computerVision.d.ts
+++ types/computerVision.d.ts
@@ -3,63 +3,14 @@
  */
 export class computerVision {
 	constructor(options: computerVisionOptions);
-
-	/**
-	 * This operation extracts a rich set of visual features based on the image content. 
-	 * Two input methods are supported -- (1) Uploading an image or (2) specifying an image URL. Within your request, there is an optional parameter to allow you to choose which features to return. By default, image categories are returned in the response. 
-	 * A successful response will be returned in JSON. If the request failed, the response will contain an error code and a message to help understand what went wrong.
-	 */
 	analyzeImage(options: AnalyzeImageOptions): Promise<AnalyzeImageReturnValue>;
-
-	/**
-	 * This operation generates a description of an image in human readable language with complete sentences. The description is based on a collection of content tags, which are also returned by the operation. More than one description can be generated for each image. Descriptions are ordered by their confidence score. All descriptions are in English. 
-	 */
 	describeImage(options: DescribeImageOptions): Promise<DescribeImageReturnValue>;
-
-	/**
-	 * This interface is used for getting handwritten text operation result. The URL to this interface should be retrieved from “Operation-Location” field returned from Recognize Handwritten Text interface.
-	 */
 	getHandwrittenTextOperationResult(options: GetHandwrittenTextOperationResultOptions): Promise<GetHandwrittenTextOperationResultReturnValue>;
-
-	/**
-	 * This operation generates a thumbnail image with the user-specified width and height. By default, the service analyzes the image, identifies the region of interest (ROI), and generates smart cropping coordinates based on the ROI. Smart cropping helps when you specify an aspect ratio that differs from that of the input image
-	 * A successful response contains the thumbnail image binary. If the request failed, the response contains an error code and a message to help determine what went wrong. 
-	 * Upon failure, the error code and an error message are returned. The error code could be one of InvalidImageUrl, InvalidImageFormat, InvalidImageSize, InvalidThumbnailSize, NotSupportedImage, FailedToProcess, Timeout, or InternalServerError. 
-	 */
 	getThumbnail(options: GetThumbnailOptions): Promise<GetThumbnailReturnValue>;
-
-	/**
-	 * This operation returns the list of domain-specific models that are supported by the Computer Vision API. Currently, the API supports following domain-specific models: celebrity recognizer, landmark recognizer. 
-	 * A successful response will be returned in JSON. If the request failed, the response will contain an error code and a message to help understand what went wrong.
-	 */
 	listDomainSpecificModels(): Promise<ListDomainSpecificModelsReturnValue>;
-
-	/**
-	 * Optical Character Recognition (OCR) detects text in an image and extracts the recognized characters into a machine-usable character stream.
-	 * Upon success, the OCR results will be returned. 
-	 * Upon failure, the error code together with an error message will be returned. The error code can be one of InvalidImageUrl, InvalidImageFormat, InvalidImageSize, NotSupportedImage, NotSupportedLanguage, or InternalServerError. 
-	 */
 	ocr(options: OCROptions): Promise<OCRReturnValue>;
-
-	/**
-	 * This operation recognizes content within an image by applying a domain-specific model. The list of domain-specific models that are supported by the Computer Vision API can be retrieved using the /models GET request. Currently, the API provides following domain-specific models: celebrities, landmarks. 
-	 * Two input methods are supported -- (1) Uploading an image or (2) specifying an image URL.
-	 * A successful response will be returned in JSON. If the request failed, the response will contain an error code and a message to help understand what went wrong.
-	 */
 	recognizeDomainSpecificContent(options: RecognizeDomainSpecificContentOptions): Promise<RecognizeDomainSpecificContentReturnValue>;
-
-	/**
-	 * Use this interface to get the result of a Recognize Handwritten Text operation. When you use the Recognize Handwritten Text interface, the response contains a field called “Operation-Location”. The “Operation-Location” field contains the URL that you must use for your Get Handwritten Text Operation Result operation. 
-	 * For the result of a Recognize Handwritten Text operation to be available, it requires an amount of time that depends on the length of the text. So, you may need to wait before using this Get Handwritten Text Operation Result interface. The time you need to wait may be up to a number of seconds. 
-	 * Note: this technology is currently in preview and is only available for English text.
-	 */
 	recognizeHandwrittenText(options: RecognizeHandwrittenTextOptions): Promise<RecognizeHandwrittenTextReturnValue>;
-
-	/**
-	 * This operation generates a list of words, or tags, that are relevant to the content of the supplied image. The Computer Vision API can return tags based on objects, living beings, scenery or actions found in images. Unlike categories, tags are not organized according to a hierarchical classification system, but correspond to image content. Tags may contain hints to avoid ambiguity or provide context, for example the tag “cello” may be accompanied by the hint “musical instrument”. All tags are in English. 
-	 * Two input methods are supported -- (1) Uploading an image or (2) specifying an image URL.
-	 * A successful response will be returned in JSON. If the request failed, the response will contain an error code and a message to help understand what went wrong.
-	 */
 	tagImage(options: TagImageOptions): Promise<TagImageReturnValue>;
 }
 
