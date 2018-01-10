6ffc66dfff051ee946b53503d11f94993ea891eb
Jeremy Foster <jeremy.foster@live.com>
2018-01-09 16:13:59 -0800

    remove method comments since they're in the source

--- src/vision/face.d.ts
+++ src/vision/face.d.ts
@@ -1,19 +1,6 @@
 export class face {
 	constructor(options: FaceOptions);
-
-	/**
-	 * Add a face to a face list. The input face is specified as an image with a targetFace rectangle. It returns a persistedFaceId representing the added face, and persistedFaceId will not expire. Note persistedFaceId is different from faceId which represents the detected face by Face - Detect. 
-	 * The persistedFaceId of face list is used in Face List - Delete a Face from a Face List to remove face from a face list, or the output JSON of Face - Find Similar 
-	 * JPEG, PNG, GIF(the first frame), and BMP are supported. The image file size should be larger than or equal to 1KB but no larger than 4MB.
-	 * The detectable face size is between 36x36 to 4096x4096 pixels. The faces out of this range will not be detected.
-	 * Rectangle specified by targetFace should contain exactly one face. Zero or multiple faces will be regarded as an error. Out of detectable face size, large head-pose, or very large occlusions will also result in fail to add a person face.
-	 * The given rectangle specifies both face location and face size at the same time. There is no guarantee of correct result if you are using rectangle which are not returned from Face - Detect. 
-	 * Face list is a group of faces, and these faces will not expire. Face list is used as a parameter of source faces in Face - Find Similar. Face List is useful when to find similar faces in a fixed face set very often, e.g. to find a similar face in a face list of celebrities, friends, or family members.
-	 * Concurrency is supported in adding or deleting faces against different face lists. Operations on a same face list will be processed sequentially. 
-	 * A face list can have a maximum of 1000 faces. 
-	 */
 	addAFaceToAFaceList(options: AddAFaceToAFaceListOptions): Promise<AddAFaceToAFaceListReturnValue>;
-
 	addAPersonFace(options: any): any;
 	createAFaceList(options: any): any;
 	createAPerson(options: any): any;
@@ -22,76 +9,16 @@ export class face {
 	deleteAFaceList(options: any): any;
 	deleteAPerson(options: any): any;
 	deleteAPersonFace(options: any): any;
-
-	/**
-	 * Delete an existing person group. Persisted face images of all people in the person group will also be deleted. 
-	 */
 	deleteAPersonGroup(options: DeleteAPersonGroupOptions): any;
-
-	/**
-	 * Detect human faces in an image and returns face locations, and optionally with faceIds, landmarks, and attributes.
-	 * Optional parameters for returning faceId, landmarks, and attributes. Attributes include age, gender, smile intensity, facial hair, head pose, glasses, emotion, hair, makeup, occlusion, accessories, blur, exposure and noise. faceId is for other APIs use including Face - Identify, Face - Verify, and Face - Find Similar. The faceId will expire 24 hours after detection call.
-	 * JPEG, PNG, GIF(the first frame), and BMP are supported. The image file size should be larger than or equal to 1KB but no larger than 4MB.
-	 * The detectable face size is between 36x36 to 4096x4096 pixels. The faces out of this range will not be detected.
-	 * A maximum of 64 faces could be returned for an image. The returned faces are ranked by face rectangle size in descending order.
-	 * Some faces may not be detected for technical challenges, e.g. very large face angles (head-pose) or large occlusion. Frontal and near-frontal faces have the best results.
-	 * Attributes (age, gender, headPose, smile, facialHair, glasses, emotion, hair, makeup, occlusion, accessories, blur, exposure and noise) are still experimental and may not be very accurate. HeadPose's pitch value is a reserved field and will always return 0.
-	 */
 	detect(options: DetectOptions): Promise<DetectReturnValue>;
-
-	/**
-	 * Given query face's faceId, to search the similar-looking faces from a faceId array or a faceListId. faceId array contains the faces created by Face - Detect, which will expire 24 hours after creation. While "faceListId" is created by Face List - Create a Face List containing persistedFaceIds that will not expire. Depending on the input the returned similar faces list contains faceIds or persistedFaceIds ranked by similarity.
-	 * Find similar has two working modes, "matchPerson" and "matchFace". "matchPerson" is the default mode that it tries to find faces of the same person as possible by using internal same-person thresholds. It is useful to find a known person's other photos. Note that an empty list will be returned if no faces pass the internal thresholds. "matchFace" mode ignores same-person thresholds and returns ranked similar faces anyway, even the similarity is low. It can be used in the cases like searching celebrity-looking faces. 
-	 */
 	findSimilar(options: FindSimilarOptions): Promise<FindSimilarReturnValue>;
-
-	/**
-	 * Retrieve a face list's information, including faceListId, name, userData and faces in the face list. Face list simply represents a list of faces, and could be treated as a searchable data source in Face - Find Similar. 
-	 */
 	getAFaceList(options: GetAFaceListOptions): Promise<GetAFaceListReturnValue>;
-
-	/**
-	 * Retrieve a person's information, including registered persisted faces, name and userData. 
-	 */
 	getAPerson(options: GetAPersonOptions): Promise<GetAPersonReturnValue>;
-
-	/**
-	 * Retrieve information about a persisted face (specified by persistedFaceId, personId and its belonging personGroupId). 
-	 */
 	getAPersonFace(options: GetAPersonFaceOptions): Promise<GetAPersonFaceReturnValue>;
-
-	/**
-	 * Retrieve the information of a person group, including its name and userData. This API returns person group information only, use Person - List Persons in a Person Group instead to retrieve person information under the person group. 
-	 */
 	getAPersonGroup(options: GetAPersonGroupOptions): Promise<GetAPersonGroupReturnValue>;
-
-	/**
-	 * Retrieve the training status of a person group (completed or ongoing). Training can be triggered by the Person Group - Train Person Group API. The training will process for a while on the server side.. 
-	 */
 	getPersonGroupTrainingStatus(options: GetPersonGroupTrainingStatusOptions): Promise<GetPersonGroupTrainingStatusReturnValue>;
-
-	/**
-	 * Divide candidate faces into groups based on face similarity. 
-	 * The output is one or more disjointed face groups and a messyGroup. A face group contains faces that have similar looking, often of the same person. Face groups are ranked by group size, i.e. number of faces. Notice that faces belonging to a same person might be split into several groups in the result. 
-	 * MessyGroup is a special face group containing faces that cannot find any similar counterpart face from original faces. The messyGroup will not appear in the result if all faces found their counterparts.
-	 * Group API needs at least 2 candidate faces and 1000 at most. We suggest to try Face - Verify when you only have 2 candidate faces.
-	 */
 	group(options: GroupOptions): Promise<GroupReturnValue>;
-
-	/**
-	 * Identify unknown faces from a person group. 
-	 * For each face in the faceIds array, Face Identify will compute similarities between the query face and all the faces in the person group (given by personGroupId), and returns candidate person(s) for that face ranked by similarity confidence. The person group should be trained to make it ready for identification. See more in Person Group - Train Person Group. 
-	 * The algorithm allows more than one face to be identified independently at the same request, but the no more than 10 faces.
-	 * Each person in the person group could have more than one face, but no more than 248 faces.
-	 * Identification works well for frontal faces and near-frontal faces.
-	 * Number of candidates returned is restricted by maxNumOfCandidatesReturned and confidenceThreshold. If no person is identified, the candidate returned will be an empty array.
-	 * Try Face - Find Similar when you need to identify similar faces from a face list instead of a person group.
-	 */
 	identify(options: IdentifyOptions): Promise<IdentifyReturnValue>;
-
-	/**
-	 * Retrieve information about all existing face lists. Only faceListId, name and userData will be returned. Try Face List - Get a Face List to retrieve face information inside faceList. 
-	 */
 	listFaceLists(): ListFaceListsReturnValue;
 	listPersonGroups(options: any): any;
 	listPersonsInAPersonGroup(options: any): any;
@@ -100,12 +27,6 @@ export class face {
 	updateAPerson(options: any): any;
 	updateAPersonFace(options: any): any;
 	updateAPersonGroup(options: any): any;
-
-	/**
-	 * Verify whether two faces belong to a same person or whether one face belongs to a person. 
-	 * This API works well for frontal and near-frontal faces. 
-	 * For the scenarios that are sensitive to accuracy please make your own judgment.
-	 */
 	verify(options: VerifyOptions): Promise<VerifyReturnValue>;
 }
 
