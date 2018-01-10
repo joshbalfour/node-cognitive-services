e8815c8da7ca12d0663c3095445d4bab8ed2f7e1
Heba Elayoty (InConsulting Inc.) <v-heelay@microsoft.com>
2018-01-10 10:16:17 -0800

    add bing types implementation

--- src/language/bingSpellCheck.d.ts
+++ src/language/bingSpellCheck.d.ts
@@ -1,4 +1,51 @@
 export class bingSpellCheck {
-	constructor(_ref: any);
-	spellCheck(_ref2: any): any;
+	constructor(options: BingSpellCheckOptions);
+	spellCheck(options: SpellCheckOptions): Promise<SpellCheckReturnValue>;
+}
+
+export interface BingSpellCheckOptions {
+	apiKey: string,
+	endpoint: string
+}
+
+export interface SpellCheckOptions {
+	parameters: SpellCheckParameters,
+	headers: SpellCheckOHeaders,
+	body: any
+}
+
+export interface SpellCheckParameters {
+	
+	/**
+	 *Mode of spellcheck:
+	 Proof - Meant to provide Office Word like spelling corrections. It can correct long queries, provide casing corrections and suppresses aggressive corrections.
+	 Spell - Meant to provide Search engine like spelling corrections. It will correct small queries(up to length 9 tokens) without any casing changes and will be more optimized (perf and relevance) towards search like queries. 
+	 */
+	mode?: string,
+	
+	/**
+	 * For proof mode, only support en-us, es-es, pt-br, For spell mode, support all language codes.
+	 */
+	mkt?: string
+
+}
+
+export interface SpellCheckOHeaders {
+	/**
+	 * Media type of the body sent to the API.
+	 */
+	"Content-Type"?: string
+}
+
+export interface SpellCheckReturnValue {
+	_type: string,
+  flaggedTokens: {
+      offset: number,
+      token: string,
+      type: string,
+      suggestions: {
+          suggestion: string,
+          score: number
+        }[]
+    }[]
 }
--- src/search/bingWebSearch.d.ts
+++ src/search/bingWebSearch.d.ts
@@ -1,4 +1,175 @@
+
 export class bingWebSearch {
-	constructor(_ref: any);
-	search(_ref2: any): any;
+	constructor(options: BingWebSearchOptions);
+	
+	/**
+	 * Get web, image, news, & videos results for a given query.
+	 */
+	search(options: SearchWebOptions): Promise<SearchWebReturnValue>;
+}
+
+export interface BingWebSearchOptions {
+	apiKey: string,
+	endpoint: string
+}
+
+export interface SearchWebOptions {
+	parameters: SearchWebParameters
+}
+
+export interface SearchWebParameters {
+	
+	/**
+	 * The user's search query string
+	 */
+	q: string,
+	
+	/**
+	 * The number of video results to return in the response. The actual number delivered may be less than requested.
+	 */
+	count: number,
+	
+	/**
+	 * The zero-based offset that indicates the number of video results to skip before returning results.
+	 */
+	offset?: number,
+	
+	/**
+	 * The market where the results come from. Typically, this is the country where the user is making the request from; however, it could be a different country if the user is not located in a country where Bing delivers results. The market must be in the form -. For example, en-US.
+	 */
+	mkt?: string,
+	
+	/**
+	 * A filter used to filter results for adult content.
+	 */
+	safeSearch?: string
+}
+
+export interface SearchWebReturnValue {
+	_type: string,
+    instrumentation: {
+        pingUrlBase: string,
+        pageLoadPingUrl: string
+    },
+    webPages: {
+        webSearchUrl: string,
+        webSearchUrlPingSuffix: string,
+        totalEstimatedMatches: number,
+        value: {
+			id: string,
+			name: string,
+			url: string,
+			urlPingSuffix: string,
+			about: {
+				name: string
+			}[],
+			displayUrl: string,
+			snippet: string,
+			deepLinks: {
+				name: string,
+				url: string,
+				urlPingSuffix: string
+			}[],
+			dateLastCrawled: string
+		}[]
+    },
+    images: {
+        id: string,
+        readLink: string,
+        webSearchUrl: string,
+        webSearchUrlPingSuffix: string,
+        isFamilyFriendly: boolean,
+        value: {
+			name: string,
+				webSearchUrl: string,
+			webSearchUrlPingSuffix: string,
+			thumbnailUrl: string,
+			datePublished: string,
+			contentUrl: string,
+			hostPageUrl: string,
+			hostPageUrlPingSuffix: string,
+			contentSize: string,
+			encodingFormat: string,
+			hostPageDisplayUrl: string,
+			width: number,
+			height: number,
+			thumbnail: {
+				width: number,
+				height: number
+			}
+		}[],
+        displayShoppingSourcesBadges: boolean,
+        displayRecipeSourcesBadges: boolean
+    },
+    news: {
+        id: string,
+        readLink: string,
+        value: {
+			name: string,
+			url: string,
+			urlPingSuffix: string,
+			image: {
+				thumbnail: {
+					contentUrl: string,
+					width: number,
+					height: number
+				}
+			},
+			description: string,
+			about: {
+				readLink: string,
+				name: string
+			}[],
+			provider: {
+				_type: string,
+				name: string
+			}[],
+			datePublished: string,
+			category: string
+		}[]
+    },
+    relatedSearches: {
+        id: string,
+        value: {
+			text: string,
+			displayText: string,
+			webSearchUrl: string,
+			webSearchUrlPingSuffix: string
+		}[],
+    },
+    videos: {
+        id: string,
+        readLink: string,
+        webSearchUrl: string,
+        webSearchUrlPingSuffix: string,
+        isFamilyFriendly: boolean,
+        value: {
+			name: string,
+			description: string,
+			webSearchUrl: string,
+			webSearchUrlPingSuffix: string,
+			thumbnailUrl: string,
+			datePublished: string,
+			publisher: {
+				name: string
+			}[],
+			contentUrl: string,
+			hostPageUrl: string,
+			hostPageUrlPingSuffix: string,
+			encodingFormat: string,
+			hostPageDisplayUrl: string,
+			width: number,
+			height: number,
+			duration: string,
+			motionThumbnailUrl: string,
+			embedHtml: string,
+			allowHttpsEmbed: boolean,
+			viewCount: number,
+			thumbnail: {
+				width: number,
+				height: number
+			}
+		}[],
+        scenario: List
+    }
 }
